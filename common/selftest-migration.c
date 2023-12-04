d6870af34aedd4e64e02a717a006e0292a0ffa7e
Nicholas Piggin <npiggin@gmail.com>
2023-12-04 17:00:09 +1000

    migration: add a migration selftest
    Add a selftest for migration support in  guest library and test harness
    code. It performs migrations in a tight loop to irritate races and bugs
    in the test harness code.
    
    Include the test in arm, s390, powerpc.
    
    Acked-by: Claudio Imbrenda <imbrenda@linux.ibm.com> (s390x)
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    Signed-off-by: Nicholas Piggin <npiggin@gmail.com>

---
+++ common/selftest-migration.c
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Machine independent migration tests
+ *
+ * This is just a very simple test that is intended to stress the migration
+ * support in the test harness. This could be expanded to test more guest
+ * library code, but architecture-specific tests should be used to test
+ * migration of tricky machine state.
+ */
+#include <libcflat.h>
+#include <migrate.h>
+
+#if defined(__arm__) || defined(__aarch64__)
+/* arm can only call getchar 15 times */
+#define NR_MIGRATIONS 15
+#else
+#define NR_MIGRATIONS 30
+#endif
+
+int main(int argc, char **argv)
+{
+	int i = 0;
+
+	report_prefix_push("migration");
+
+	for (i = 0; i < NR_MIGRATIONS; i++)
+		migrate_quiet();
+
+	report(true, "simple harness stress test");
+
+	report_prefix_pop();
+
+	return report_summary();
+}
